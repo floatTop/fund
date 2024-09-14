c86169eb317fcba533b3fce60d203f7f90fda444
liqiujiang <1554994006@qq.com>
2024-09-14 20:46:49 +0800

    fix: add log

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,6 +2,7 @@ import RootTable from "@/components/page/root/table";
 import { getFundList } from "./api";
 
 export default async function Page() {
+  console.log('getFundList')
   const res = await getFundList().catch((e) => {
     console.log("catch", e);
     return [];
