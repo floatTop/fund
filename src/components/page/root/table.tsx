a78bbbd09f3a4e93999a226e38b163afceff371c
liqiujiang <1554994006@qq.com>
2024-09-15 23:30:43 +0800

    refactor: 调整按钮布局并添加市场API端点
    调整按钮布局并添加市场API端点

--- src/components/page/root/table.tsx
+++ src/components/page/root/table.tsx
@@ -58,7 +58,7 @@ export default function RootTable({ fundList }: { fundList: FundList[] }) {
           add market
         </Button>
       </div>
-
+----
       <ProTable
         title="我的自选"
         columns={columns}
